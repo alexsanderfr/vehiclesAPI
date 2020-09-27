587e44e57466f0be9725da307b3dee97945d292d
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2020-09-27 19:37:35 -0300

    Add swagger

---
+++ api/src/main/java/com/udacity/vehicles/configuration/DocumentationConfig.java
@@ -0,0 +1,35 @@
+package com.udacity.vehicles.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class DocumentationConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Car API",
+                "This API returns a list of cars.",
+                "1.0",
+                "http://www.udacity.com/tos",
+                new Contact("Alexsander Franco", "www.udacity.com", "myeaddress@udacity.com"),
+                "License of API", "http://www.udacity.com/license", Collections.emptyList());
+    }
+}
