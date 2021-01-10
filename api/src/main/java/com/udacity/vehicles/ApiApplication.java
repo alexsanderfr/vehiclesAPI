4a3302ea313d3135420aec2b01a92ea35559713d
Alexsander Franco <alexsanderfrancorosa@gmail.com>
2021-01-09 21:27:28 -0300

    Change dependencies

--- api/src/main/java/com/udacity/vehicles/ApiApplication.java
+++ api/src/main/java/com/udacity/vehicles/ApiApplication.java
@@ -19,7 +19,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableSwagger2
 public class ApiApplication {
 
     public static void main(String[] args) {
